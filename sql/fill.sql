02cfbe65424fb0245c634cf1b1e663e78f66de84
rponomarev <rponomarev@webinar.ru>
2024-06-12 23:15:21 +0300

    lab-6 ('fixed sql')

--- sql/fill.sql
+++ sql/fill.sql
@@ -2,7 +2,7 @@ CREATE TABLE public.stocks_customuser
 (
     id  SERIAL NOT NULL PRIMARY KEY,
     email    VARCHAR(254),
-    password VARCHAR(50),
+    password VARCHAR(254),
     is_staff    BOOLEAN DEFAULT false,
     is_superuser    BOOLEAN DEFAULT false,
     last_login TIMESTAMP
@@ -17,6 +17,7 @@ CREATE TABLE public.stocks_orderevent
 
 GRANT ALL ON ALL TABLES IN SCHEMA public to postgres;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO postgres;
+GRANT CREATE ON SCHEMA public to postgres
 
 ALTER TABLE public.stocks_customuser
 ALTER COLUMN password TYPE VARCHAR(254);
